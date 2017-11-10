7b1b73fbe029dd8a38e846ab11c7536a0907a9b5
Stephen Mark <stephenjmark@gmail.com>
2017-11-10 07:27:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,13 +42,12 @@ function viewCart() {
         i++
       }
 
-      if (cartCount === 2){
+      if (cartCount === 2) {
         var sentence =  `In your cart, you have ${itemNameAndPrice.join(' ')}`
       } else{
         var sentence =  `In your cart, you have ${itemNameAndPrice.join(', ')}`
       }
-
-      var sentence =  `In your cart, you have ${itemNameAndPrice.join(', ')}`
+      
       console.log(sentence)
     }
 
