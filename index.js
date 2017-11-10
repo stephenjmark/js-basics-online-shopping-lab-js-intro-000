d50f8c461a2fd2b59ce79b90ca108faf31f74c6a
Stephen Mark <stephenjmark@gmail.com>
2017-11-10 05:55:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,24 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * (100))
+  cart.push(new Object({[item]: price }))
+  console.log(`${item} has been added to your cart.`)
+  return cart
 }
 
 function viewCart() {
-  // write your code here
+  var cartCount = getCart().length
+
+  if(cartCount === 0){
+    console.log('Your shopping cart is empty.')
+  } else {
+      if (cartCount === 1) {
+      var cartItems = Object.keys(cart);
+      var itemPrice = cart[cartItems[0]];
+      console.log(`In your cart, you have ${cartItems[0]} at $${itemPrice}.`)
+    }
+  }
 }
 
 function total() {
