ff2c26c19f83767e242b74c5bd1acc47c651b3f2
Stephen Mark <stephenjmark@gmail.com>
2017-11-10 06:48:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function viewCart() {
   } else {
       var itemName = Object.keys(cart[0])
       var itemPrice = Object.values(cart[0]);
-      
+
       console.log(`In your cart, you have ${itemName} at $${itemPrice)}.`)
     }
 
